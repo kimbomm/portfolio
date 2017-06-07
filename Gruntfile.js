5daffc51a2c19725bdc6d0c9174cb495aa5210f4
kimbomm <qhals0710@gmail.com>
2017-06-07 16:18:45 +0900

    first commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,73 @@ module.exports = function(grunt) {
   // 프로젝트 환경설정.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    serve:{
+      options:{
+        port:9000
+      }
+    },
+
+    less: {
+      product: {
+        options: {
+          paths: ['assets/less']
+        },
+        files: {
+          'assets/css/index.css': 'assets/less/index.less'
+        }
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      development: {
+        options: {
+          paths: ['assets/less'],
+          dumpLineNumbers:"comments"
+        },
+        files: {
+          'assets/css/index.dev.css': 'assets/less/index.less'
+        }
+      }
+    },
+
+
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'assets/css/output.css': ['assets/css/index.css']
+        }
+      }
+    },
+
+    watch: {
+      less: {
+        files: 'assets/less/*.less',
+        tasks: ['less'],
+        options: {
+          spaw:false
+        }
+      },
+      cssmin:{
+        files: 'assets/css/index.css',
+        tasks: ['cssmin'],
+        options: {
+          spaw:false
+        }
       }
     }
+
+
   });
 
-  // "uglify" task를 지원하는 플러그인 로드.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // 플러그인 로드.
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.loadNpmTasks('grunt-serve');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['serve']);
 
 };
